b10ee9850816238a22b09648921efc5246145267
Matthew Iannucci <mpiannucci@gmail.com>
2016-11-27 21:37:34 -0500

    First commit with working pretty Navbar

--- BuoyFinder/AppDelegate.swift
+++ BuoyFinder/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Google Maps Auth
+        GMSServices.provideAPIKey("AIzaSyB5StrjG3MFFOVVdd9y3c1AxNEyU1La1X4")
+        
         return true
     }
 
