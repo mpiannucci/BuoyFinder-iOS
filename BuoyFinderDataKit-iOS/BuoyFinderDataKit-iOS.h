eed162c185b135ef16a867fabcf0661278626ddb
Matthew Iannucci <mpiannucci@gmail.com>
2016-11-28 16:25:09 -0500

    Add pod lock and new gitignore, along with empty buoyfinder framework

---
+++ BuoyFinderDataKit-iOS/BuoyFinderDataKit-iOS.h
@@ -0,0 +1,18 @@
+//
+//  BuoyFinderDataKit-iOS.h
+//  BuoyFinderDataKit-iOS
+//
+//  Created by Matthew Iannucci on 11/28/16.
+//  Copyright © 2016 Matthew Iannucci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BuoyFinderDataKit-iOS.
+FOUNDATION_EXPORT double BuoyFinderDataKit_iOSVersionNumber;
+
+//! Project version string for BuoyFinderDataKit-iOS.
+FOUNDATION_EXPORT const unsigned char BuoyFinderDataKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuoyFinderDataKit_iOS/PublicHeader.h>
+#include "BuoyFinderDataKit.h"
---
+++ BuoyFinderDataKit/BuoyFinderDataKit.h
@@ -0,0 +1,13 @@
+//
+//  BuoyFinderDataKit.h
+//  BuoyFinder
+//
+//  Created by Matthew Iannucci on 11/28/16.
+//  Copyright © 2016 Matthew Iannucci. All rights reserved.
+//
+
+#ifndef BuoyFinderDataKit_h
+#define BuoyFinderDataKit_h
+
+
+#endif /* BuoyFinderDataKit_h */
