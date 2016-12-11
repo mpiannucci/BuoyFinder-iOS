5cc780d8d3903aebec85871d4712c5a797b440a0
Matthew Iannucci <mpiannucci@gmail.com>
2016-12-11 16:23:13 -0500

    Change structs to classes so they are encodable

--- BuoyFinderDataKit/Location.swift
+++ BuoyFinderDataKit/Location.swift
@@ -8,12 +8,39 @@
 
 import Foundation
 
-struct Location {
-    
+class Location: NSCoding {
     var latitude: Double
     var longitude: Double
     
     // Optional
     var altitude: Double?
     var locationName: String?
+    
+    init(latitude: Double, longitude: Double, altitude: Double? = nil, locationName: String? = nil) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.locationName = locationName
+    }
+    
+    // MARK: NSCoding
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        if let lat = aDecoder.decodeObject(forKey: "latitude") as? Double,
+            let lon = aDecoder.decodeObject(forKey: "longitude") as? Double {
+            self.init(latitude: lat, longitude: lon)
+        } else {
+            return nil
+        }
+        
+        self.altitude = aDecoder.decodeObject(forKey: "altitude") as? Double
+        self.locationName = aDecoder.decodeObject(forKey: "locationName") as? String
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.latitude, forKey: "latitude")
+        aCoder.encode(self.longitude, forKey: "longitude")
+        aCoder.encode(self.altitude, forKey: "altitude")
+        aCoder.encode(self.locationName, forKey: "locationName")
+    }
 }
--- BuoyFinderDataKit/Swell.swift
+++ BuoyFinderDataKit/Swell.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwiftyJSON
 
-struct Swell {
+class Swell: NSCoding {
     var waveHeight: Double
     var period: Double
     var direction: Double?
@@ -29,6 +29,14 @@ struct Swell {
         }
     }
     
+    init (waveHeight: Double, period: Double, direction: Double? = nil, compassDirection: String? = nil, units: Units) {
+        self.waveHeight = waveHeight
+        self.period = period
+        self.direction = direction
+        self.compassDirection = compassDirection
+        self.units = units
+    }
+    
     init (jsonData: JSON) {
         waveHeight = jsonData["WaveHeight"].doubleValue
         period = jsonData["Period"].doubleValue
@@ -36,17 +44,41 @@ struct Swell {
         compassDirection = jsonData["CompassDirection"].stringValue
         units = Units(rawValue: jsonData["Units"].stringValue)!
     }
+    
+    // MARK: NSCoding
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        if let height = aDecoder.decodeObject(forKey: "waveHeight") as? Double,
+            let per = aDecoder.decodeObject(forKey: "period") as? Double,
+            let unit = aDecoder.decodeObject(forKey: "units") as? Units {
+            self.init(waveHeight: height, period: per, units: unit)
+        } else {
+            return nil
+        }
+        
+        self.direction = aDecoder.decodeObject(forKey: "direction") as? Double
+        self.compassDirection = aDecoder.decodeObject(forKey: "compassDirection") as? String
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.waveHeight, forKey: "waveHeight")
+        aCoder.encode(self.period, forKey: "period")
+        aCoder.encode(self.direction, forKey: "direction")
+        aCoder.encode(self.compassDirection, forKey: "compassDirection")
+        aCoder.encode(self.units, forKey: "units")
+    }
+    
 }
 
 extension Swell: UnitsProtocol {
 
     // Assumes everything is in english -> going to metric
-    public mutating func convertToMetric() {
+    public func convertToMetric() {
         self.waveHeight = Units.feetToMeters(feetValue: self.waveHeight)
     }
     
     // Assumes everything is in metric -> going to english
-    public mutating func convertToEnglish() {
+    public func convertToEnglish() {
         self.waveHeight = Units.metersToFeet(metricValue: self.waveHeight)
     }
 }
