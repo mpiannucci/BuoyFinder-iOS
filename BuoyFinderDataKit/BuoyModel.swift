49ed220801f5c1463c84e7973dd030e6147804a2
Matthew Iannucci <mpiannucci@gmail.com>
2016-12-14 21:03:27 -0500

    Add buoymodel

---
+++ BuoyFinderDataKit/BuoyModel.swift
@@ -0,0 +1,19 @@
+//
+//  BuoyModel.swift
+//  BuoyFinder
+//
+//  Created by Matthew Iannucci on 12/14/16.
+//  Copyright © 2016 Matthew Iannucci. All rights reserved.
+//
+
+import UIKit
+
+class BuoyModel: NSObject {
+    
+    static let sharedModel = BuoyModel()
+    
+    private override init() {
+        
+    }
+
+}
