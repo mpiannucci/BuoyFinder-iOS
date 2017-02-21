47ee59780f0a6348f51fc76ab18540b0ca744788
Matthew Iannucci <mpiannucci@gmail.com>
2017-02-20 21:49:39 -0500

    Favorites table working, realtime favorites updating causes crashes

--- BuoyFinder/FavoriteBuoysViewController.swift
+++ BuoyFinder/FavoriteBuoysViewController.swift
@@ -12,35 +12,48 @@ class FavoriteBuoysViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.tableView.reloadData()
+        
+        NotificationCenter.default.addObserver(self.tableView, selector: #selector(self.tableView.reloadData), name: SyncManager.syncDataUpdatedNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return SyncManager.instance.favoriteBuoys.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteBuoyCell", for: indexPath)
 
-        // Configure the cell...
+        let buoy = SyncManager.instance.favoriteBuoys[indexPath.row]
+        cell.textLabel?.text = buoy.fullName
+        cell.detailTextLabel?.text = "Station: " + buoy.stationID + " " + buoy.program!
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
