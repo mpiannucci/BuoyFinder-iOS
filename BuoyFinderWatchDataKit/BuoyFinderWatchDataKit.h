0849b8b78a99a1a50c3605ee268c95454d9665aa
Matthew Iannucci <mpiannucci@gmail.com>
2017-05-20 16:32:59 -0400

    Add watch framework target

---
+++ BuoyFinderWatchDataKit/BuoyFinderWatchDataKit.h
@@ -0,0 +1,19 @@
+//
+//  BuoyFinderWatchDataKit.h
+//  BuoyFinderWatchDataKit
+//
+//  Created by Matthew Iannucci on 5/20/17.
+//  Copyright © 2017 Matthew Iannucci. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BuoyFinderWatchDataKit.
+FOUNDATION_EXPORT double BuoyFinderWatchDataKitVersionNumber;
+
+//! Project version string for BuoyFinderWatchDataKit.
+FOUNDATION_EXPORT const unsigned char BuoyFinderWatchDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuoyFinderWatchDataKit/PublicHeader.h>
+
+
